78ed5338d80a6b1ea059bd24bc0bba58ed673da3
Edwin Noriega <adolfonoriega9@gmail.com>
2025-06-22 12:09:00 -0600

    feat: enhance IncomingSesDto and related classes with validation decorators for improved data integrity

--- src/mapper/dto/incoming-ses.dto.ts
+++ src/mapper/dto/incoming-ses.dto.ts
@@ -1,4 +1,6 @@
 import { AutoMap } from '@automapper/classes';
+import { Type } from 'class-transformer';
+import { IsArray, IsDefined, IsObject, ValidateNested } from 'class-validator';
 
 export class Verdict {
   @AutoMap()
@@ -13,18 +15,38 @@ export class Receipt {
   processingTimeMillis: number;
 
   @AutoMap(() => Verdict)
+  @ValidateNested()
+  @Type(() => Verdict)
+  @IsDefined()
+  @IsObject()
   spamVerdict: Verdict;
 
   @AutoMap(() => Verdict)
+  @ValidateNested()
+  @Type(() => Verdict)
+  @IsDefined()
+  @IsObject()
   virusVerdict: Verdict;
 
   @AutoMap(() => Verdict)
+  @ValidateNested()
+  @Type(() => Verdict)
+  @IsDefined()
+  @IsObject()
   spfVerdict: Verdict;
 
   @AutoMap(() => Verdict)
+  @ValidateNested()
+  @Type(() => Verdict)
+  @IsDefined()
+  @IsObject()
   dkimVerdict: Verdict;
 
   @AutoMap(() => Verdict)
+  @ValidateNested()
+  @Type(() => Verdict)
+  @IsDefined()
+  @IsObject()
   dmarcVerdict: Verdict;
 }
 
@@ -36,23 +58,39 @@ export class Mail {
   source: string;
 
   @AutoMap(() => [String])
+  @IsArray()
   destination: string[];
 }
 
 export class Ses {
   @AutoMap(() => Receipt)
+  @ValidateNested()
+  @Type(() => Receipt)
+  @IsDefined()
+  @IsObject()
   receipt: Receipt;
 
   @AutoMap(() => Mail)
+  @ValidateNested()
+  @Type(() => Mail)
+  @IsDefined()
+  @IsObject()
   mail: Mail;
 }
 
 export class SesRecord {
   @AutoMap(() => Ses)
+  @ValidateNested()
+  @Type(() => Ses)
+  @IsDefined()
+  @IsObject()
   ses: Ses;
 }
 
 export class IncomingSesDto {
   @AutoMap(() => [SesRecord])
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => SesRecord)
   Records: SesRecord[];
 }
