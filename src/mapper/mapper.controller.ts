88e2040745c4892ba685299934d802006b6a3907
Edwin Noriega <adolfonoriega9@gmail.com>
2025-06-22 12:09:32 -0600

    feat: implement mapping endpoint in MapperController to process IncomingSesDto and return ProcessedEmailDto

--- src/mapper/mapper.controller.ts
+++ src/mapper/mapper.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UsePipes,
+} from '@nestjs/common';
+import { MapperService } from './mapper.service';
+import { IncomingSesDto } from './dto/incoming-ses.dto';
+import { ProcessedEmailDto } from './dto/processed-email.dto';
 
 @Controller('mapper')
-export class MapperController {}
+export class MapperController {
+  constructor(private readonly mapperService: MapperService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe({ transform: true }))
+  map(@Body() incomingDto: IncomingSesDto): ProcessedEmailDto {
+    return this.mapperService.process(incomingDto);
+  }
+}
