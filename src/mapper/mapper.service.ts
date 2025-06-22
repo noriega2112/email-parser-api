7af9d17f6adaad9119c3473e54e389b25eccbfc1
Edwin Noriega <adolfonoriega9@gmail.com>
2025-06-22 12:01:33 -0600

    feat: implement process method in MapperService to map IncomingSesDto to ProcessedEmailDto

--- src/mapper/mapper.service.ts
+++ src/mapper/mapper.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectMapper } from '@automapper/nestjs';
+import { Mapper } from '@automapper/core';
+import { IncomingSesDto, SesRecord } from '@/mapper/dto/incoming-ses.dto';
+import { ProcessedEmailDto } from '@/mapper/dto/processed-email.dto';
 
 @Injectable()
-export class MapperService {}
+export class MapperService {
+  constructor(@InjectMapper() private readonly mapper: Mapper) {}
+
+  process(incomingDto: IncomingSesDto): ProcessedEmailDto {
+    const sesRecord = incomingDto.Records[0];
+    return this.mapper.map(sesRecord, SesRecord, ProcessedEmailDto);
+  }
+}
