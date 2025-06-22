b00d028eee86e13b4cabc8cf6ef67020ce137518
Edwin Noriega <adolfonoriega9@gmail.com>
2025-06-22 12:00:04 -0600

    feat: integrate AutomapperModule and update MapperModule to include MapperProfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MapperModule } from './mapper/mapper.module';
+import { AutomapperModule } from '@automapper/nestjs';
+import { classes } from '@automapper/classes';
 
 @Module({
-  imports: [MapperModule],
+  imports: [
+    MapperModule,
+
+    AutomapperModule.forRoot({
+      strategyInitializer: classes(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/mapper/mapper.module.ts
+++ src/mapper/mapper.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MapperController } from './mapper.controller';
 import { MapperService } from './mapper.service';
+import { MapperProfile } from './mapper.profile';
 
 @Module({
   controllers: [MapperController],
-  providers: [MapperService]
+  providers: [MapperService, MapperProfile],
 })
 export class MapperModule {}
